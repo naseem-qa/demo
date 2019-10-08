e114b3c13cc3b635dba34338e398d84342a9bec5
naseem <izzatnaseem30@gmail.com>
2019-10-09 00:54:49 +0300

    finaly

--- js/lab2.js
+++ js/lab2.js
@@ -1,70 +1,70 @@
 
 'use strict';
+var score =0;
 var msg = prompt('What is your name?');
 alert('hello' + ' ' + msg);
-console.log('hello' , msg);
+console.log('hello', msg);
 
-var nam = prompt('Do you know who is Naseem?');
-name = nam.toLowerCase();
+var questions = [
 
-if (nam == 'yes') {
-  alert('welecom1!');
-}
-else if (nam == 'no') {
-  alert('try to know some thing about Naseem !');
-}
-else {
-  alert('try to know some thing about Naseem !');
-}
+    ['Do you know who is Naseem?', 'welecom1!', 'try to know some thing about Naseem !'],
+    ['Do you know where Naseem live?', 'great', 'that not good !'],
+    ['Do you know my specilaization ?', 'I am happy to hear that.', 'that is not problem!'],
+    ['Do you know naseem current job ?', 'you are close!', 'don\'t worry!. come on'],
+    ['Do you know any things about Naseem phobia ?', 'she has aphobia from reptile and insect!', 'This is good for you!'],
 
-var city = prompt('Do you know where Naseem live?');
-city = city.toLocaleLowerCase();
-if (city == 'yes') {
-    alert('great');
-}
-else if (city == 'no ') {
-    alert('that not good !');
-}
-else {
-    alert('that not good !');
-}
+]
 
-var spec = prompt('Do you know my specilaization ?');
-spec = spec.toLocaleLowerCase();
-if (spec == 'yes') {
-    alert('I am happy to hear that.');
-}
-else if (spec == 'no') {
-    alert('that is not problem!');
-}
-else {
-    alert('that is not problem!');
+for (var i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i];
+    var currentPrompt = currentQuestion[0]
+    var userResponse = prompt(currentPrompt)
+    userResponse = userResponse.toLowerCase();
+    var currentPositiveReply = currentQuestion[1];
+    var currentNegativeReply = currentQuestion[2];
+    switch (userResponse) {
+        case 'yes':
+            alert(currentPositiveReply);
+            score++;
+            break;
+        case 'no':
+            alert(currentNegativeReply);
+            break;
+        default:
+            alert(currentNegativeReply);
+
+    }
 }
 
 
-var job = prompt('Do you know naseem current job ?');
-job = job.toLocaleLowerCase();
-if (job == 'yes') {
-    alert('you are close!');
-}
-else if (job == 'no') {
-    alert('don\'t worry!. come on');
-}
-else {
-    alert('don\'t worry! come on');
+for (var j = 1; j < 5; j++) {
+    var ansewr = prompt('Gusse Naseem univeristy grade?! ');
+    if (ansewr > 80) {
+        alert(' your ansewr is too high ');
+    } else if (ansewr < 80) {
+        alert(' your ansewr is too low');
+    } else if (ansewr == 80) {9
+        alert('that is it');
+        score++;
+        j = 5;
+    }
+ }
+
+var favoritedishes = [ 'see food', 'mansaf ', 'fettuccin','burger','tabola','green salad'];
+
+for ( var k=0 ; k<6 ; k++ ) {
+    var favoritefood = prompt ('can you gusse some of my faverite dishes?');
+    favoritefood = favoritefood.toLocaleLowerCase()
+
+for ( var j=0 ; j<6 ; j++){
+    if (  favoritefood === favoritedishes [k]){
+        alert (' yes this is one of my favorite dishes!');
+        score++ ;
+    } 
+} 
 }
 
-var phobia = prompt('Do you know any things about Naseem phobia ?');
-phobia = phobia.toLocaleLowerCase();
-if (phobia == 'yes') {
-    alert('she has aphobia from reptile and insect!');
-}
-else if (phobia == 'no') {
-    alert('This is good for you!');
-}
-else {
-    alert('This is good for you! ');
-}
 
+alert( ' your score ='+ ' '+ score );
+alert('Thank you for searching about me' +' '+ msg);
 
-alert('Thank you for searching about me' + msg);
